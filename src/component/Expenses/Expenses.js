9ad8fcdd6e0468920dbae789dd5d73ee1a31dae6
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2024-01-04 17:44:15 +0530

    React Rendering problem filter

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -6,18 +6,21 @@ import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} 
         onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense)=>(
+        {filteredExpenses.map((expense)=>(
           <ExpenseItem
           key={expense.id}
           title={expense.title}
